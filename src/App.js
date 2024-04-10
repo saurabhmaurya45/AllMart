5e38d0cdd14d15cdd0420ee2ee2d59d6c1abad58
saurabhmaurya <tgtsaurabhmaurya45@gmail.com>
2024-04-10 11:53:40 +0530

    fix: fixed scroll problem

--- src/App.js
+++ src/App.js
@@ -5,23 +5,18 @@ import './App.css';
 import Navbar from './components/navbar/navbar';
 import Footer from './components/footer/footer';
 import { wishListLength, cartLength } from "./contextApi/navbarValues";
-import { useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import ScrollToTop from "./pages/scrollToTopPage";
 
 export default function App() {
   const wishList = localStorage.getItem('wishListData') ? JSON.parse(localStorage.getItem('wishListData')) : [];
   const cart = localStorage.getItem('cartData') ? JSON.parse(localStorage.getItem('cartData')) : [];
   const [wishListLengthValue, setWishListLengthValue] = React.useState(wishList?.length);
   const [cartLengthValue, setCartLengthValue] = React.useState(cart?.length);
-  let location = useLocation()
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [location])
   return (
     <wishListLength.Provider value={[wishListLengthValue, setWishListLengthValue]} >
       <cartLength.Provider value={[cartLengthValue, setCartLengthValue]} >
         <div className="App">
-          {/* <ScrollToTop /> */}
+          <ScrollToTop />
           <Navbar />
           <Outlet />
           <Footer />
